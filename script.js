88ddf58647af3160b0660ad88b2b277a50ce533b
Dinesh Dhayfule <109637254+dineshdhayfule@users.noreply.github.com>
2023-11-12 11:58:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
         document.querySelector("p.Player1").innerHTML = player1; 
         document.querySelector("p.Player2").innerHTML = player2; 
     } 
-
+editNames();
 let startButton = document.getElementById('button');
 let startButton1 = document.getElementById('button1');
 let startButton2 = document.getElementById('btreset');
